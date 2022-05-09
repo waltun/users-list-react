68cd49eb505a63ba4c15407e554e532e6db869f5
waltun <pooriyamostaan1@gmail.com>
2022-05-09 21:32:26 +0430

    Create UI table of users list

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import Users from "./Users";
 function App() {
   return (
     <div className="max-w-4xl mx-auto font-IRANSans">
+      <div className="mt-10">
+        <p className="text-2xl font-bold text-center">لیست کاربران</p>
+      </div>
+
       <Users />
     </div>
   );
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,77 @@
 import React from "react";
 
 function Users() {
-    return (
-        <div>
-            <p className="text-center font-bold">
-                لیست کاربران
-            </p>
-        </div>
-    )
+  return (
+    <div className="mt-6">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="px-4 py-3 text-center text-sm text-gray-900"
+            >
+              #
+            </th>
+            <th
+              scope="col"
+              className="px-4 py-3 text-center text-sm text-gray-900"
+            >
+              نام
+            </th>
+            <th
+              scope="col"
+              className="px-4 py-3 text-center text-sm text-gray-900"
+            >
+              شماره تماس
+            </th>
+            <th
+              scope="col"
+              className="px-4 py-3 text-center text-sm text-gray-900"
+            >
+              ایمیل
+            </th>
+            <th
+              scope="col"
+              className="px-4 py-2 text-center text-sm text-gray-900"
+            >
+              شماره ملی
+            </th>
+            <th scope="col" className="relative px-6 py-3">
+              <span className="sr-only">اقدامات</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          <tr className="hover:bg-gray-100">
+            <td className="px-4 py-3 text-center text-gray-700">
+              <p className="text-sm">1</p>
+            </td>
+            <td className="px-4 py-3 text-center text-gray-700">
+              <p className="text-sm">پوریا مستعان</p>
+            </td>
+            <td className="px-4 py-3 text-center text-gray-700">
+              <p className="text-sm">09022228553</p>
+            </td>
+            <td className="px-4 py-3 text-center text-gray-700">
+              <p className="text-sm">pooriya@gmail.com</p>
+            </td>
+            <td className="px-4 py-3 text-center text-gray-700">
+              <p className="text-sm">0430142821</p>
+            </td>
+            <td className="px-4 py-3 space-x-2 space-x-reverse">
+              <button className="px-4 py-1 rounded-md bg-indigo-500 text-white hover:bg-indigo-600 text-sm">
+                ویرایش
+              </button>
+                <button className="px-4 py-1 rounded-md bg-red-500 text-white hover:bg-red-600 text-sm">
+                  حذف
+                </button>
+
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Users;
