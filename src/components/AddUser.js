daa54b6c8c9c7362f12ff938bf90092d2d19f03c
waltun <pooriyamostaan1@gmail.com>
2022-05-10 10:10:00 +0430

    Create form for add user in modal

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationIcon, PlusIcon } from "@heroicons/react/outline";
+import { PlusIcon, UserAddIcon } from "@heroicons/react/outline";
 
 function AddUser() {
   const [open, setOpen] = useState(false);
@@ -20,7 +20,7 @@ function AddUser() {
       <Transition.Root show={open} as={Fragment}>
         <Dialog
           as="div"
-          className="relative z-10"
+          className="relative z-10 font-IRANSans"
           initialFocus={cancelButtonRef}
           onClose={setOpen}
         >
@@ -38,7 +38,6 @@ function AddUser() {
 
           <div className="fixed z-10 inset-0 overflow-y-auto">
             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-              {/* This element is to trick the browser into centering the modal contents. */}
               <span
                 className="hidden sm:inline-block sm:align-middle sm:h-screen"
                 aria-hidden="true"
@@ -54,46 +53,127 @@ function AddUser() {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <Dialog.Panel className="relative inline-block align-bottom bg-white rounded-lg text-right overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl w-full">
                   <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="sm:flex sm:items-start">
+                    <div className="sm:flex sm:items-center">
                       <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <ExclamationIcon
-                          className="h-6 w-6 text-red-600"
+                        <UserAddIcon
+                          className="h-6 w-6 text-indigo-600"
                           aria-hidden="true"
                         />
                       </div>
-                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <div className="mt-3 text-center sm:mt-0 sm:mr-4 sm:text-right">
                         <Dialog.Title
                           as="h3"
-                          className="text-lg leading-6 font-medium text-gray-900"
+                          className="text-md leading-6 font-medium text-gray-900"
                         >
-                          Deactivate account
+                          افزودن کاربر جدید
                         </Dialog.Title>
-                        <div className="mt-2">
-                          <p className="text-sm text-gray-500">
-                            Are you sure you want to deactivate your account?
-                            All of your data will be permanently removed. This
-                            action cannot be undone.
-                          </p>
-                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-4 sm:grid grid-cols-2 gap-4">
+                      <div className="mb-4 sm:mb-0">
+                        <label
+                          className="text-sm font-bold mb-2 block"
+                          for="inputName"
+                        >
+                          نام
+                        </label>
+                        <input
+                          type="text"
+                          id="inputName"
+                          name="name"
+                          className="w-full bg-white px-4 py-2 text-sm border border-gray-400 rounded-md"
+                        />
+                      </div>
+                      <div className="mb-4 sm:mb-0">
+                        <label
+                          className="text-sm font-bold mb-2 block"
+                          for="inputEmail"
+                        >
+                          ایمیل
+                        </label>
+                        <input
+                          type="email"
+                          name="email"
+                          id="inputEmail"
+                          className="w-full bg-white px-4 py-2 text-sm border border-gray-400 rounded-md"
+                        />
+                      </div>
+                      <div className="mb-4 sm:mb-0">
+                        <label
+                          className="text-sm font-bold mb-2 block"
+                          for="inputPhone"
+                        >
+                          شماره تماس
+                        </label>
+                        <input
+                          type="text"
+                          name="phone"
+                          id="inputPhone"
+                          className="w-full bg-white px-4 py-2 text-sm border border-gray-400 rounded-md"
+                        />
+                      </div>
+                      <div className="mb-4 sm:mb-0">
+                        <label
+                          className="text-sm font-bold mb-2 block"
+                          for="inputNation"
+                        >
+                          کد ملی
+                        </label>
+                        <input
+                          type="text"
+                          name="nation"
+                          id="inputNation"
+                          className="w-full bg-white px-4 py-2 text-sm border border-gray-400 rounded-md"
+                        />
+                      </div>
+                      <div className="mb-4 sm:mb-0">
+                        <label
+                          className="text-sm font-bold mb-2 block"
+                          for="inputGender"
+                        >
+                          جنسیت
+                        </label>
+                        <select
+                          className="w-full bg-white px-4 py-2 text-sm border border-gray-400 rounded-md"
+                          id="inputGender"
+                        >
+                          <option>مرد</option>
+                          <option>زن</option>
+                        </select>
+                      </div>
+                      <div className="mb-4 sm:mb-0">
+                        <label
+                          className="text-sm font-bold mb-2 block"
+                          for="inputType"
+                        >
+                          نوع کاربر
+                        </label>
+                        <select
+                          className="w-full bg-white px-4 py-2 text-sm border border-gray-400 rounded-md"
+                          id="inputType"
+                        >
+                          <option>مدیریت</option>
+                          <option>معمولی</option>
+                        </select>
                       </div>
                     </div>
                   </div>
                   <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button
                       type="button"
-                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                      onClick={() => setOpen(false)}
-                    >
-                      انصراف
-                    </button>
-                    <button
-                      type="button"
-                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                      className="w-full inline-flex justify-center rounded-md shadow-sm px-4 py-2 bg-green-500 text-base font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:mt-0 sm:w-auto sm:text-sm"
                       ref={cancelButtonRef}
                     >
                       ثبت کاربر
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 sm:mt-0 w-full inline-flex justify-center rounded-md shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={() => setOpen(false)}
+                    >
+                      انصراف
                     </button>
                   </div>
                 </Dialog.Panel>
