8e57c70bacc1806093cf8659f04fd85da86588e8
waltun <pooriyamostaan1@gmail.com>
2022-05-11 14:23:14 +0430

    Create tableHeader component and import to users.js

---
+++ src/components/tableHeader.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function TableHeader(props) {
+  return (
+    <tr>
+      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
+        #
+      </th>
+      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
+        نام
+      </th>
+      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
+        ایمیل
+      </th>
+      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
+        شماره تماس
+      </th>
+      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
+        تاریخ ثبت
+      </th>
+      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
+        نوع کاربر
+      </th>
+      <th className="relative px-6 py-3">
+        <span className="sr-only">اقدامات</span>
+      </th>
+    </tr>
+  );
+}
+
+export default TableHeader;
--- src/components/users.js
+++ src/components/users.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TableHeader from "./tableHeader";
 import UserItems from "./userItems";
 
 function Users() {
@@ -6,29 +7,7 @@ function Users() {
     <main className="my-4">
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
-          <tr>
-            <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-              #
-            </th>
-            <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-              نام
-            </th>
-            <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-              ایمیل
-            </th>
-            <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-              شماره تماس
-            </th>
-            <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-              تاریخ ثبت
-            </th>
-            <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-              نوع کاربر
-            </th>
-            <th className="relative px-6 py-3">
-              <span className="sr-only">اقدامات</span>
-            </th>
-          </tr>
+          <TableHeader />
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           <UserItems />
