e7f6c999460764015f6e81168a1d089e0850a23a
SPY_STAR <91047452+SPY-STAR0003@users.noreply.github.com>
2022-05-15 12:25:49 +0430

    simplify td & th tags

--- src/components/tableHeader.js
+++ src/components/tableHeader.js
@@ -1,28 +1,19 @@
 import React from "react";
 
 function TableHeader(props) {
+
+    let thTableClass = "px-4 py-3 text-center text-sm text-gray-900 font-bold";
+
   return (
     <tr>
-      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-        #
-      </th>
-      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-        نام
-      </th>
-      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-        ایمیل
-      </th>
-      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-        شماره تماس
-      </th>
-      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-        تاریخ ثبت
-      </th>
-      <th className="px-4 py-3 text-center text-sm text-gray-900 font-bold">
-        نوع کاربر
-      </th>
+      <th className={thTableClass}> # </th>
+      <th className={thTableClass}> نام </th>
+      <th className={thTableClass}>ایمیل </th>
+      <th className={thTableClass}> شماره تماس </th>
+      <th className={thTableClass}> تاریخ ثبت </th>
+      <th className={thTableClass}> نوع کاربر </th>
       <th className="relative px-6 py-3">
-        <span className="sr-only">اقدامات</span>
+        <span className="sr-only">  اقدامات</span>
       </th>
     </tr>
   );
--- src/components/userItems.js
+++ src/components/userItems.js
@@ -5,7 +5,10 @@ import EditUserForm from "./editUserForm";
 
 function UserItems({ item, index, setUsers, users }) {
 
-  const [edit, setEdit] = useState(false)
+
+  const [edit, setEdit] = useState(false);
+
+  let tdTableClass = "px-4 py-2 whitespace-nowrap text-center text-gray-700 text-sm";
 
   return (
     <>
@@ -19,29 +22,12 @@ function UserItems({ item, index, setUsers, users }) {
         />
       ) : (
         <tr className="hover:bg-gray-100">
-          <td className="px-4 py-2 whitespace-nowrap text-center text-gray-700">
-            <p className="text-sm">{index + 1}</p>
-          </td>
-          <td className="px-4 py-2 whitespace-nowrap text-center text-gray-700">
-            <p className="text-sm">{item.name}</p>
-          </td>
-          <td className="px-4 py-2 whitespace-nowrap text-center text-gray-700">
-            <p className="text-sm">{item.email}</p>
-          </td>
-          <td className="px-4 py-2 whitespace-nowrap text-center text-gray-700">
-            <p className="text-sm">{item.phone}</p>
-          </td>
-          <td
-            className="px-4 py-2 whitespace-nowrap text-center text-gray-700"
-            dir="ltr"
-          >
-            <p className="text-sm">{item.date}</p>
-          </td>
-          <td className="px-4 py-2 whitespace-nowrap text-center text-gray-700">
-            <p className="text-sm">
-              {item.type === "normal" ? "کاربر معمولی" : "کاربر ادمین"}
-            </p>
-          </td>
+          <td className={tdTableClass}> {index + 1} </td>
+          <td className={tdTableClass}> {item.name} </td>
+          <td className={tdTableClass}> {item.email} </td>
+          <td className={tdTableClass}> {item.phone} </td>
+          <td className={tdTableClass}> {item.date} </td>
+          <td className={tdTableClass}> {item.type === "normal" ? "کاربر معمولی" : "کاربر ادمین"} </td>
           <td className="px-4 py-2 whitespace-nowrap space-x-2 space-x-reverse">
             <EditUser
               index={index}
